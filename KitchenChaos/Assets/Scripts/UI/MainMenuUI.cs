d6d77bc0c9f1585c6b42ba4f7fff4303b839c051
VAHANI VATSKUMAR RASIKBHAI <98044806+vats0007@users.noreply.github.com>
2024-06-29 16:51:37 +0530

    Menu And Loading Added

---
+++ KitchenChaos/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() => {
+            //Click
+            Loader.Load(Loader.Scene.GameScene);
+        });
+        quitButton.onClick.AddListener(() => {
+            //Click
+            Application.Quit();
+        });
+    }
+
+    
+}
